1e38a56f1ca7e20d7f101372c4f1bfe01639af78
andrecreppe <andre.creppe@gmail.com>
2021-04-06 16:28:14 -0300

    Importing tensorflow and making detections

--- src/App.js
+++ src/App.js
@@ -6,10 +6,77 @@ import Webcam from 'react-webcam';
 import './App.css';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runHandpose = async () => {
+    const net = await handpose.load();
+    console.log('Handpose model loaded');
+
+    // Loop and detect hands
+    const timer = 100; // ms
+    setInterval(() => {
+      detect(net);
+    }, timer);
+  }
+
+  const detect = async (net) => {
+    // Check data is available
+    if(
+      typeof webcamRef.current !== 'undefined' &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // Get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+      
+      // Set video and canvas dimensions
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Detect
+      const hand = await net.estimateHands(video);
+      // console.log(hand);
+
+      // Draw mesh
+    }
+  }
+
+  runHandpose();
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Webcam ref={webcamRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }} />
+
+        <canvas ref={canvasRef}
+        style={{
+          position: 'absolute',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          left: 0,
+          right: 0,
+          textAlign: 'center',
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }}></canvas>
       </header>
     </div>
   );

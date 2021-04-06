6b33028568f947441d4ba12c66bc3c837894a769
andrecreppe <andre.creppe@gmail.com>
2021-04-06 17:41:55 -0300

    Emoji gesture identifier
    Manipulate the gesture result to show an image

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import Webcam from 'react-webcam';
 import * as fp from 'fingerpose';
 import { drawHand } from './utilities';
 
-import peace from './images/peace.png'
+import victory from './images/victory.png'
 import thumbs from './images/thumbs.png'
 import './App.css';
 
 function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
+  
+  const [emoji, setEmoji] = useState(null);
+  const images = {
+    thumbs_up: thumbs,
+    victory: victory
+  };
 
   const runHandpose = async () => {
     const net = await handpose.load();
-    console.log('Handpose model loaded');
+    // console.log('Handpose model loaded');
 
     // Loop and detect hands
     const timer = 100; // ms
@@ -49,14 +55,28 @@ function App() {
 
       // Gesture detection
       if(hand.length > 0) {
+        // Gestures to be detected
         const GE = new fp.GestureEstimator([
           fp.Gestures.VictoryGesture,
           fp.Gestures.ThumbsUpGesture
         ]);
 
-        const confidence = 8;
-        const gesture = await GE.estimate(hand[0].landmarks, confidence);
-        console.log(gesture);
+        const minConfidence = 8;
+        const gesture = await GE.estimate(hand[0].landmarks, minConfidence);
+        // console.log(gesture);
+
+        if(gesture.gestures !== undefined && gesture.gestures.length > 0) {
+          const confidence = gesture.gestures.map((pred) => {
+            return pred.confidence;
+          });
+
+          const maxConf = confidence.indexOf(
+            Math.max.apply(null, confidence)
+          );
+
+          setEmoji(gesture.gestures[maxConf].name);
+          console.log(images[emoji]);
+        }
       }
 
       // Draw mesh
@@ -70,31 +90,51 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Webcam ref={webcamRef}
-        style={{
-          position: 'absolute',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          left: 0,
-          right: 0,
-          textAlign: 'center',
-          zIndex: 9,
-          width: 640,
-          height: 480
-        }} />
+        <Webcam 
+          ref={webcamRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }} 
+        />
 
-        <canvas ref={canvasRef}
-        style={{
-          position: 'absolute',
-          marginLeft: 'auto',
-          marginRight: 'auto',
-          left: 0,
-          right: 0,
-          textAlign: 'center',
-          zIndex: 9,
-          width: 640,
-          height: 480
-        }}></canvas>
+        <canvas 
+          ref={canvasRef}
+          style={{
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left: 0,
+            right: 0,
+            textAlign: 'center',
+            zIndex: 9,
+            width: 640,
+            height: 480
+          }} 
+        />
+
+        { emoji !== null ? 
+          <img 
+            src={images[emoji]} 
+            style={{
+              position: 'absolute',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              left: 400,
+              bottom: 500,
+              zIndex: 10,
+              right: 0,
+              textAlign: 'center',
+              height: 100
+            }} 
+          /> : '' }
       </header>
     </div>
   );
